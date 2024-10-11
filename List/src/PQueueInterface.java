5b867f65ed25cf034eea10e3aca7196b5893de76
B00835054 <abbyrea2002@gmail.com>
2024-10-11 14:10:14 +0100

    push

---
+++ List/src/PQueueInterface.java
@@ -0,0 +1,24 @@
+public interface PQueueInterface<T>{
+
+    public void enqueue(T newEntry);
+    /* adds new entry to the linked queue in its priority position
+     */
+    public T dequeue();
+    /* removes entry from the queue
+     */
+
+    public T getFront();
+    /*gets item from the front of the queue
+     */
+
+
+    public boolean isEmpty();
+    /* returns a boolean value which determines if the queue is empty of not
+     */
+
+    public void clear();
+    /* clears all items from the queue
+     */
+}
+
+
